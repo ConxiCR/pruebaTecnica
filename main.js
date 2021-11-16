03b78983565c921e74e55d45ab6038e1064e0ef4
ConxiCr <conxicr@hotmail.com>
2021-11-16 13:10:35 +0100

    resolución prueba técnica
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

---
+++ main.js
@@ -0,0 +1,10 @@
+//algoritmo que ordene un vector de números de forma descendente
+const numeros = [3, 24, 12, 53, 45, 33];
+numeros.sort((a, b) => b - a);
+//algoritmo que transforme un array de arrays en un array unidimensional
+let colores = ['azul', 'rojo', 'amarillo'];
+colores.splice(0, 2);
+
+//algoritmo que cuente la cantidad de palabras que contiene una cadena de texto
+let cadena = 'Hola que tal';
+cadenaCantidad = cadena.length;
